d5f1f4cbef4db2a63f7f0bf4e8bd689e10d950ff
Adrian Taylor <33820513+Hadrianix@users.noreply.github.com>
2020-10-21 16:10:41 -0600

    Created

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function error() {
+    
+        var animation = bodymovin.loadAnimation({
+            container: document.getElementById('bm'),
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+            path: '/js/404.json'
+        });
+
+    
+
+}
